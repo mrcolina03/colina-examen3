df087d844e4a99097af75cd034e62c4a3afa02fc
Mateo Ruben Colina Morales <mrcolina@espe.edu.ec>
2026-02-06 17:11:18 -0500

    proyecto creado

---
+++ src/main/java/ec/edu/espe/colinaexamen3/ColinaExamen3Application.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.colinaexamen3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColinaExamen3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColinaExamen3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ec/edu/espe/colinaexamen3/ColinaExamen3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.colinaexamen3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColinaExamen3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
