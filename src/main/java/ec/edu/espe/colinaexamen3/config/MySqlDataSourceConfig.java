7669c5d075f0651f7a3f9ea41bd08bcb2e54af2f
Mateo Ruben Colina Morales <mrcolina@espe.edu.ec>
2026-02-06 17:58:09 -0500

    v1

--- src/main/java/ec/edu/espe/colinaexamen3/config/MySqlDataSourceConfig.java
+++ src/main/java/ec/edu/espe/colinaexamen3/config/MySqlDataSourceConfig.java
@@ -9,6 +9,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -21,18 +22,21 @@ import org.springframework.transaction.PlatformTransactionManager;
         transactionManagerRef = "mysqlTransactionManager")
 public class MySqlDataSourceConfig {
 
+    @Primary
     @Bean
     @ConfigurationProperties("spring.datasource.mysql")
     public DataSourceProperties mysqlDataSourceProperties() {
         return new DataSourceProperties();
     }
 
+    @Primary
     @Bean
     public DataSource mysqlDataSource(
             @Qualifier("mysqlDataSourceProperties") DataSourceProperties properties) {
         return properties.initializeDataSourceBuilder().build();
     }
 
+    @Primary
     @Bean
     public LocalContainerEntityManagerFactoryBean mysqlEntityManagerFactory(
             EntityManagerFactoryBuilder builder,
@@ -45,6 +49,7 @@ public class MySqlDataSourceConfig {
                 .build();
     }
 
+    @Primary
     @Bean
     public PlatformTransactionManager mysqlTransactionManager(
             @Qualifier("mysqlEntityManagerFactory") LocalContainerEntityManagerFactoryBean factory) {
